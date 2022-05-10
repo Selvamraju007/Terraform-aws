edb9020119f2dd29be69939687e29c94ea92484c
sheersh2001 <67103008+sheersh2001@users.noreply.github.com>
2022-05-10 10:19:49 +0530

    Update awswith1publicand1privatesubnet.tf

--- three/awswith1publicand1privatesubnet.tf
+++ three/awswith1publicand1privatesubnet.tf
@@ -55,26 +55,25 @@ resource "aws_instance" "web" {
 
 resource "aws_security_group" "sg" {
   name        = "sgterraform"
-  description = "Allow TLS inbound traffic"
   vpc_id      = aws_vpc.main.id
 
-  ingress {
-    description      = "httpallow"
-    from_port        = 22
-    to_port          = 22
+  dynamic "ingress" {
+    for_each = var.ports
+    content{
+    from_port        = ingress.value
+    to_port          = ingress.value
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"] 
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+    }
+  } 
+  ingress {
+    from_port        = -1
+    to_port          = -1
+    protocol         = "icmp"
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-  ingress {
-    description      = "httpallow"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"] 
-    ipv6_cidr_blocks = ["::/0"]
-  }
- 
   egress {
     from_port        = 0
     to_port          = 0
